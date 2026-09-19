a875cf6354895730d04bd8237aeb2f17d279c4dd
skatesuv-glitch <skatesuv@gmail.com>
2026-09-19 09:38:25 +0200

    Add Citroen auth routing and separate remote access session

---
+++ app/src/main/java/com/ec3control/network/StellantisHeaders.kt
@@ -0,0 +1,7 @@
+package com.ec3control.network
+
+object StellantisHeaders {
+    const val REALM_HEADER = "x-introspect-realm"
+    const val ACCEPT = "application/hal+json"
+    const val USER_AGENT = "okhttp/4.12.0"
+}
