1d3cc2b78ec5eb59180534ab2b577887132cb8c2
skatesuv-glitch <skatesuv@gmail.com>
2026-09-20 08:14:57 +0200

    Fix OTP session member line break

--- app/src/main/java/com/ec3control/data/stellantis/StellantisOtpNetwork.kt
+++ app/src/main/java/com/ec3control/data/stellantis/StellantisOtpNetwork.kt
@@ -11,7 +11,8 @@ import javax.xml.parsers.DocumentBuilderFactory
 
 data class OtpNetworkResult(val ok:Boolean,val message:String)
 
-class StellantisOtpNetwork(private val http:OkHttpClient=OkHttpClient()){\n private var activeOtp:StellantisOtpActivation?=null
+class StellantisOtpNetwork(private val http:OkHttpClient=OkHttpClient()){
+ private var activeOtp:StellantisOtpActivation?=null
  suspend fun activate(accessToken:String,smsCode:String,pin:String):OtpNetworkResult=withContext(Dispatchers.IO){
   require(accessToken.length>=16){"OAuth token unavailable"}
   require(smsCode.isNotBlank()){"SMS code required"}
