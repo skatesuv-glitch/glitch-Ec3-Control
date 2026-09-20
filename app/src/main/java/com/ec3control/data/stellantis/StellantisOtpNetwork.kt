9d94c03813193ad1e7afbf0b3b53bcdcc4905b79
skatesuv-glitch <skatesuv@gmail.com>
2026-09-20 10:36:06 +0200

    Fix JSON array import for MQTT association lookup

--- app/src/main/java/com/ec3control/data/stellantis/StellantisOtpNetwork.kt
+++ app/src/main/java/com/ec3control/data/stellantis/StellantisOtpNetwork.kt
@@ -9,6 +9,7 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonPrimitive
 import org.w3c.dom.Element
 import java.io.ByteArrayInputStream
